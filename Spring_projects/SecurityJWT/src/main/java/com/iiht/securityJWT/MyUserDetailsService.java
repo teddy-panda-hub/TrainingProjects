5e889edc36fe4e6f651bb971a354e3f348276e15
teddy-panda-hub <teddypanda123456789@gmail.com>
2024-09-19 11:37:53 +0530

    Add files via upload

---
+++ Spring_projects/SecurityJWT/src/main/java/com/iiht/securityJWT/MyUserDetailsService.java
@@ -0,0 +1,21 @@
+package com.iiht.securityJWT;
+
+import java.util.ArrayList;
+
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+	@Override
+	public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {	
+
+	System.out.println(userName);
+	return new org.springframework.security.core.userdetails.User("foo","foo",new ArrayList<>());
+	}
+
+}
---
+++ Spring_projects/firstRestJPA_MySQL_H2/src/main/java/com/iiht/restJPA/MYSQL/FirstRestJpaMySqlApplication.java
@@ -0,0 +1,14 @@
+package com.iiht.restJPA.MYSQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstRestJpaMySqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstRestJpaMySqlApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
